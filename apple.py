6de25c30874abbdb6a37d7114c015a8a85b1fb12
ddorn <diego.dorn@free.fr>
2019-01-17 13:11:56 +0100

    option to save a map + ap in a separate class

--- apple.py
+++ apple.py
@@ -3,7 +3,9 @@
 """
 Another Perfect Lite Level Editor
 """
+import json
 from functools import lru_cache
+from typing import List
 
 import pygame
 from graphalama.app import App, Screen
@@ -58,28 +60,88 @@ class Tile:
         return pygame.transform.scale(img, (self.size * scale, self.size * scale))
 
 
+class Map(dict):
+
+    def __init__(self, tiles=(), **kwargs):
+        self.tiles = tiles  # type: List[Tile]
+
+        super().__init__(**kwargs)
+
+    def save(self, file='assets/levels/0'):
+        to_save = {}
+        for pos, tile in self.items():
+            to_save[f'{pos[0]}, {pos[1]}'] = tile
+        s = json.dumps(to_save, indent=4)
+        with open(file, 'w') as f:
+            f.write(s)
+
+    @classmethod
+    def load(cls, file='assets/levels/0'):
+        with open(file, 'r') as f:
+            d = json.loads(f.read())
+
+        map_ = cls()
+        for pos_string, tile in d.items():
+            x, y = map(int, pos_string.split())
+            map_[(x, y)] = tile
+
+        return map_
+
+    def get_image_at(self, pos, scale=4):
+        tile_index = self[pos]
+        tile = self.tiles[tile_index]
+        image = tile.get_image(self.get_neighbors(pos), scale)
+        return image
+
+    def get_neighbors(self, pos):
+        """Get a table with the neighbors. topleft will be table[-1][-1] and center right will be table[1][0]."""
+        neigh = [[None] * 3 for _ in range(3)]
+
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                neighbor = pos[0] + x, pos[1] + y
+                tile = self.get(neighbor, None)
+                neigh[x][y] = tile
+
+        return tuple(tuple(line) for line in neigh)
+
+
 class EditScreen(Screen):
     BRUSH = 1
     ERASER = 2
 
     def __init__(self, app):
 
+        # widgets
         self.tool_carousel = CarouselSwitch(["Brush", "Eraser"], self.tool_change, (20, 20))
-        reset = Button("Reset", self.reset, bg_color=(240, 60, 60))
+        reset = Button("Reset", self.reset, bg_color=(240, 100, 60))
         save = Button("Save", self.save)
         widgets = (self.tool_carousel, reset, save)
         super().__init__(app, widgets, (20, 40, 90))
-        self.map = {}
+
+        # map
         self.tile_size = 16
-        self.tiles = [Tile("assets/dirt_sheet.png", self.tile_size)]
-        self.tile_index = 0
+        tiles = [Tile("assets/dirt_sheet.png", self.tile_size)]
+        self.map = Map(tiles)
+        self._tile_index = 0
+
+        # editor settings
         self.scale = 4
         self.drawing = False
         self.tool = self.BRUSH
 
+
+    @property
+    def tile_index(self):
+        return self._tile_index
+
+    @tile_index.setter
+    def tile_index(self, value):
+        self._tile_index = value % len(self.map.tiles)
+
     @property
     def current_tile(self):
-        return self.tiles[self.tile_index % len(self.tiles)]
+        return self.map.tiles[self.tile_index]
 
     def tool_change(self, tool_name: str):
         self.tool = getattr(self, tool_name.upper(), self.BRUSH)
@@ -118,8 +180,9 @@ class EditScreen(Screen):
 
         # draw tiles
         for pos, tile_i in self.map.items():
-            tile = self.tiles[tile_i]
-            image = tile.get_image(self.get_neighbors(pos), self.scale)
+            # tile = self.tiles[tile_i]
+            image = self.map.get_image_at(pos, self.scale)
+            # image = tile.get_image(self.get_neighbors(pos), self.scale)
             display.blit(image, self.pos_to_screen(pos))
 
         self.widgets.render(display)
@@ -132,23 +195,11 @@ class EditScreen(Screen):
         tot_scale = self.tile_size * self.scale
         return screen[0] // tot_scale, screen[1] // tot_scale
 
-    def get_neighbors(self, pos):
-        """Get a table with the neighbors. topleft will be table[-1][-1] and center right will be table[1][0]."""
-        neigh = [[None] * 3 for _ in range(3)]
-
-        for x in range(-1, 2):
-            for y in range(-1, 2):
-                neighbor = pos[0] + x, pos[1] + y
-                tile = self.map.get(neighbor, None)
-                neigh[x][y] = tile
-
-        return tuple(tuple(line) for line in neigh)
-
     def reset(self):
-        self.map = {}
+        self.map.clear()
 
     def save(self):
-        pass
+        self.map.save()
 
 
 class Apple(App):
